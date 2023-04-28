fcb82f6a7041f17190b41b06776cc217030a9800
diazan6 <diazan6@gmail.com>
2023-04-28 13:21:40 -0700

    Created routes and requestTime. Added http-errors and nodemon. Added static folder with working favicon.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
+const path = require("path");
+const createError = require("http-errors");
+
 const express = require("express");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.static(path.join(".", "backend", "static")));
+
+const rootRoutes = require("../backend/routes/root.js");
+const requestTime = require("../backend/middleware/request-time.js");
+
+app.use(requestTime);
+app.use("/", rootRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
+});
+
+app.use((req, res, next) => {
+  next(createError(404));
 });
