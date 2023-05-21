45cf0c9a497415d3d842b362f4a36c7dd651c7da
diazan6 <diazan6@gmail.com>
2023-05-21 00:49:01 -0700

    Finished basic chat with sockets.

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,34 @@
-const { io } = require("socket.io-client");
+import io from "socket.io-client";
 
 console.log("Hello from a bundle.");
-io();
+const socket = io();
+
+const messageContainer = document.querySelector("#chat-container");
+socket.on("chat-message", ({ message, sender }) => {
+  const display = document.createElement("div");
+  const name = document.createElement("span");
+  name.innerText = sender;
+
+  const thing = document.createElement("div");
+  thing.innerText = message.message;
+  display.appendChild(name);
+  display.appendChild(thing);
+
+  messageContainer.appendChild(display);
+});
+
+document
+  .querySelector("input[id=chatMessage]")
+  .addEventListener("keydown", (event) => {
+    if (event.keyCode === 13) {
+      const message = event.target.value;
+      console.log(message);
+      event.target.value = "";
+
+      fetch("/chat/0", {
+        method: "post",
+        body: JSON.stringify({ message }),
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+  });
